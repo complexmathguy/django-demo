916ddeff7a803bc9bec37979be3194811fc1f02c
Steven Randolph <steven.randolph@realmethods.com>
2021-05-23 22:03:48 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 cd /var/www/
 python manage.py migrate --run-syncdb
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@harbormaster.ai', '')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@harbormaster.ai', 'letmein2')"
 python manage.py runserver 0.0.0.0:8080
